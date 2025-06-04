d3c91365210c18341e59dd0cdf9eb7f444e19eaf
uzairahmed90-JD <usmanalichanna90@gmail.com>
2025-06-04 17:31:44 +0500

    Delete issue_book directory

--- issue_book/src/main/java/com/repository/IssueBookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.repository;
-
-import com.tut.common.entity.IssueBook;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IssueBookRepository extends JpaRepository<IssueBook,Long> {
-}
