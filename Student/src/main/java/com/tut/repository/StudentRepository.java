1c270a935a77a4989e38dd479b70144172da5cd6
uzairahmed90-JD <usmanalichanna90@gmail.com>
2025-06-04 17:30:32 +0500

    Delete Student directory

--- Student/src/main/java/com/tut/repository/StudentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.tut.repository;
-import com.tut.common.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentRepository extends JpaRepository<Student,Long> {
-}
